aa0e984cb7c417fdb832517e8cec9cfb7d5bac4c
mayday46 <81172807+Mayday46@users.noreply.github.com>
2024-12-27 02:08:02 +0000

    Final

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Entry() {
-  return;
+// 1. Create a template for the repeated code.
+
+function Entry(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.descrpition}</dd>
+    </div>
+  );
 }
 
 export default Entry;
